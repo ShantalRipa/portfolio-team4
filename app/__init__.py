ed890bd65713e345fe1ed5475560bd643a3744a6
Cloud User <centos@ip-172-31-29-107.us-west-1.compute.internal>
2021-06-30 18:38:36 +0000

    Add existing file

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,24 @@
 import os
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, request, render_template, send_from_directory, Response, jsonify
 from dotenv import load_dotenv
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from app.db import get_db
 
 load_dotenv()
 app = Flask(__name__)
-
+app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
+db.init_app(app)
 
 @app.route('/')
 def index():
     # Ji-Oh just change the home.html to index.html to see your index.html
     return render_template('home.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/health')
+def starting_url():
+    status_code = Response(status=200)
+    return status_code
 
 @app.route('/aboutus')
 def aboutus():
